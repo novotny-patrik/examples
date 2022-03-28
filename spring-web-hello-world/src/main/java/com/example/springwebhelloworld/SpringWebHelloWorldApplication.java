357818e4d2987189f72b07df08828e67883de905
patrik <novotny.patrik@post.cz>
2022-03-28 21:42:52 +0200

    Javadoc

--- spring-web-hello-world/src/main/java/com/example/springwebhelloworld/SpringWebHelloWorldApplication.java
+++ spring-web-hello-world/src/main/java/com/example/springwebhelloworld/SpringWebHelloWorldApplication.java
@@ -3,6 +3,9 @@ package com.example.springwebhelloworld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Entry point
+ */
 @SpringBootApplication
 public class SpringWebHelloWorldApplication {
 
